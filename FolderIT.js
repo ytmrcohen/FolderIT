9cce4524b8a80fe8b38b4ccc398a266d503129a7
ytmrcohen <ytmr454@gmail.com>
2025-12-10 21:05:08 +0200

    Improve readability with comments in FolderIT.js
    Refactor and add comments to the login and signup functionality.

--- FolderIT.js
+++ FolderIT.js
@@ -1,3 +1,4 @@
+#variables_of_login_and_sign_up_boxes    
 const Confirmsignup = document.getElementById("savebutton");
 const signup = document.getElementById("passwordinput");
 
@@ -8,9 +9,7 @@ const result = document.getElementById("result");
 const Show = document.getElementById("showbox");
 const Showlog = document.getElementById("showboxlog");
 
-
-
-
+#reveals_the_login_and_signup_boxes_on_button_press   
 Show.addEventListener("click", () => {
     signupbox.style.display = "flex";
 });
@@ -18,16 +17,15 @@ Showlog.addEventListener("click", () => {
     signinbox.style.display = "flex";
 });
 
-
-
-
+#gets_user_password_in_signup
 let userPassword = "";
 
 Confirmsignup.addEventListener("click", function() {
     userPassword = signup.value.trim();
     result.textContent = "Signup completed succesfully";
 });
- 
+
+#checks_in_login_if_it_matches_the_signup
 Confirmlogin.addEventListener("click", function() {
     const signinValue = signin.value.trim();
 
@@ -37,4 +35,5 @@ Confirmlogin.addEventListener("click", function() {
     }else{
         result.textContent = "Wrong Username or Password";
     }
+
 });
